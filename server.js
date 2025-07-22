a6acc371d33222b5e887851f8917c1262d2dc78e
Yathish Acharya <yathishkumar2013@gmail.com>
2025-07-22 22:53:54 +0530

    added cors

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://grojetdelivery.com',
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // Basic Route
