097d1fafb494c482eac4b5bf14ae254dab4c8c29
Yathish Acharya <yathishkumar2013@gmail.com>
2025-08-13 22:34:33 +0530

    removed heath token issue

--- routes/tools/networkhealth.js
+++ routes/tools/networkhealth.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { protect } from '../../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Example route to check network health
-router.get('/', protect, (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({
         status: 'ok',
         message: 'Network health is good',
